f0ffcded2542d12776cf9ffd4f0cf98e0f08da50
hiscaler <hiscaler@gmail.com>
2018-12-29 13:10:13 +0800

    ...

---
+++ src/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"os"
+	"io/ioutil"
+	"encoding/json"
+)
+
+type Config struct {
+	DocumentDir string
+}
+
+func NewConfig() *Config {
+	config := &Config{}
+
+	if file, err := os.Open("./config/config.json"); err != nil {
+		config.DocumentDir = ""
+	} else {
+		defer file.Close()
+		jsonByte, err := ioutil.ReadAll(file)
+		if err == nil {
+			json.Unmarshal(jsonByte, &config)
+		}
+	}
+
+	return config
+}
