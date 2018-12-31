be33c5064cbca38e6ca170dd9870bb35e5f9afe9
hiscaler <hiscaler@gmail.com>
2018-12-31 11:16:11 +0800

    解析文档目录（WIP）

--- src/server.go
+++ src/server.go
@@ -1,9 +1,33 @@
 package main
 
-func init() {
+import (
+	"config"
+	"io/ioutil"
+	"fmt"
+	"path"
+	"strings"
+)
 
+var cfg *config.Config
+
+func init() {
+	cfg = config.NewConfig()
 }
 
 func main() {
+	docs := make([]map[string]string, 0)
+	dirs, _ := ioutil.ReadDir(cfg.DocumentDir)
+	for _, dir := range dirs {
+		if dir.IsDir() {
+			mdDocs := make(map[string]string)
+			files, _ := ioutil.ReadDir(path.Join(cfg.DocumentDir, dir.Name()))
+			for _, file := range files {
+				filename := strings.TrimSuffix(path.Base(file.Name()), path.Ext(file.Name()))
+				mdDocs[filename] = path.Join(cfg.DocumentDir, dir.Name(), file.Name())
+			}
+			docs = append(docs, mdDocs)
+		}
+	}
 
+	fmt.Println(fmt.Sprintf("%#v", docs))
 }
