55e1b5665555fba7a5d30e0d88e8931b8b52339c
hiscaler <hiscaler@gmail.com>
2018-12-31 14:21:49 +0800

    ...

--- src/server.go
+++ src/server.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"path"
 	"strings"
+	"doc"
+	"os"
+	"encoding/json"
 )
 
 var cfg *config.Config
@@ -15,19 +18,45 @@ func init() {
 }
 
 func main() {
-	docs := make([]map[string]string, 0)
+	docs := make([]doc.Doc, 0)
 	dirs, _ := ioutil.ReadDir(cfg.DocumentDir)
 	for _, dir := range dirs {
 		if dir.IsDir() {
-			mdDocs := make(map[string]string)
+			section := doc.Section{}
+			doc := doc.Doc{}
+			file, err := os.Open(path.Join(cfg.DocumentDir, dir.Name(), "conf.json"))
+			if err == nil {
+				jsonByte, err := ioutil.ReadAll(file)
+				if err == nil {
+					json.Unmarshal(jsonByte, &doc)
+				}
+				file.Close()
+			}
+			doc.ModifyDatetime = dir.ModTime()
+			if len(doc.Title) == 0 {
+				doc.Title = dir.Name()
+			}
+			if len(doc.Author) == 0 {
+				doc.Author = "Unknown"
+			}
+
 			files, _ := ioutil.ReadDir(path.Join(cfg.DocumentDir, dir.Name()))
 			for _, file := range files {
-				filename := strings.TrimSuffix(path.Base(file.Name()), path.Ext(file.Name()))
-				mdDocs[filename] = path.Join(cfg.DocumentDir, dir.Name(), file.Name())
+				if file.Name() != "conf.json" {
+					filename := strings.TrimSuffix(path.Base(file.Name()), path.Ext(file.Name()))
+					section.Title = filename
+					f, _ := os.Open(path.Join(cfg.DocumentDir, dir.Name(), file.Name()))
+					c, _ := ioutil.ReadAll(f)
+					section.Content = string(c)
+					doc.Sections = append(doc.Sections, section)
+				}
 			}
-			docs = append(docs, mdDocs)
+			docs = append(docs, doc)
 		}
 	}
 
-	fmt.Println(fmt.Sprintf("%#v", docs))
+	for _, doc := range docs {
+		fmt.Println("Doc name is ", doc.Title)
+		fmt.Println(fmt.Sprintf("%#v", doc))
+	}
 }
